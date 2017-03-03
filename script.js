5fdc94d32656ccb2618dbd254f17d7fec8e4aea5
Florent Tornil <flo_tornil34@hotmail.fr>
2017-03-03 15:16:42 +0100

    Fusion code (C/F) + beau <3
    Fusion du code de claire et florent et modifications pour structurer le code et les fonctions

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
 // Evenement déclenché lorsque le DOM a fini de charger, qui va appeller notre fonction setup()
-document.addEventListener("DOMContentLoaded", presetup);
+document.addEventListener("DOMContentLoaded", setup);
 
-var body; // variable correspondant au body, servant à simplifier la lecture du code
-var terrain; // variable correspondant au terrain de jeu (div), servant à simplifier la lecture du code
+var body;				// variable correspondant au body, servant à simplifier la lecture du code
+var terrain; 			// variable correspondant au terrain de jeu (div), servant à simplifier la lecture du code
+var fond; 				// variable correspondant au fond (img), servant à simplifier la lecture du code
+var joueur; 			// variable correspondant au joueur (img), servant à simplifier la lecture du code
+var ballon; 			// variable correspondant au ballon (img), servant à simplifier la lecture du code
+var scores; 			// variable correspondant aux 5 meilleurs scores (div), servant à simplifier la lecture du code
+var regles; 			// variable correspondant aux regles (div), servant à simplifier la lecture du code
+var boutonCacherRegles	// variable correspondant au bouton permettant de cacher les règles du jeu (input), servant à simplifier la lecture du code
+var TONscore; 			// variable correspondant au score actuel du joueur (div), servant a simplifier la lecture du code
 
-var fond; // variable correspondant au fond (img), servant à simplifier la lecture du code
-var joueur; // variable correspondant au joueur (img), servant à simplifier la lecture du code
-var ballon; // variable correspondant au ballon (img), servant à simplifier la lecture du code
-var regles= document.getElementById('regles'); //on recupère la div qui va afficher les règles 
+var afficherEcranAccueil = true;	// booleen vrai ssi il faut afficher l'écran d'accueil dans le setup au lieu de l'image de fond du terrain
+var afficherEcranPerdu = false;		// boolean vrai ssi il faut afficher l'écran gameOver dans le setup au lieu de l'image de fond du terrain
 
-var nbDeplacement; //compte le nombre de déplacement du personnage, on l'initialise à zero
+var nbDeplacement; 	// nombre de déplacement du personnage
+var score;  		// score actuel du joueur
+var nbVie; 			// nombre de vies restantes au joueur ( = nombres de panier ouvant etre ratées avant d'avoir perdu )
 
-var score=0;  //variable comptant le score
-var TONscore=document.getElementById('tonScore');
+var enCoursDeTir = false; 	// vrai ssi le joueur est en train de tirer (touche enfoncée)
 
+// fonction d'initialisation du jeu
+function setup() {
+ 
+    // on initialise les variables globales body et terrain
+    body = document.getElementById('body');
+    terrain = document.getElementById('terrain');
 
-var nbVie=5;
+	// au cas où ce n'est pas la première fois que la fonction est appellée, on "nettoie" le terrain, en supprimant tout ses enfants dans le DOM
+	var nbChildren = terrain.children.length //on compte les enfants de la zone
+	for(var i=0; i < nbChildren; i++) {
+		terrain.removeChild(terrain.lastChild); //on supprime les enfants de la zone
+	}
 
+    // on enleve le margin par defaut du body qui décale légèrement le jeu vers la droite et le bas
+    body.style.margin = "0px";
 
-// variables concernant le tir
-var enCoursDeTir = false; // vrai ssi le joueur est en train de tirer
+	//
+	// Initialisation des variables déplacement et vies
+	//
 
-var tempsDebutTir; // le timeStamp de l'event
+    nbDeplacement=0;
+	if(afficherEcranAccueil) {
+		score = 0;
+		nbVie = 5;
+	}
 
-//fonction permettant de charger une image de fond 
-function chargerImage(img) {
-	terrain = document.getElementById('terrain');
+    //
+    //  Fond
+    //
+	
 	var imageFond  = document.createElement("img");
-    imageFond.src="./"+img; 
-    imageFond.id = img;
+	if(afficherEcranAccueil) {
+		imageFond.src = "./index.png";
+	}
+	else if(afficherEcranPerdu) {
+		imageFond.src = "./game.png";
+	}
+	else {
+		imageFond.src = "./fond.jpg";
+	}
+
+    imageFond.id = "fond";
     terrain.appendChild(imageFond);
 
-    fond = document.getElementById(img);
+    fond = document.getElementById('fond');
 
     // on redimensionne le fond du terrain de jeu
     fond.width = 2 * window.innerWidth / 3;
@@ -39,39 +72,6 @@ function chargerImage(img) {
 
     // on positionne le plateau en absolu afin de pouvoir si besoin est, modifier sa position avec les propriétés CSS "top" et "left"
     fond.style.position = "absolute";
-    
-}
-//fonction permettant de commencer le feu
-function presetup() {
-	//chargement de l'écran de départ
-	chargerImage('index.png'); 
-	//événement à l'écoute d'une action sur une touche du clavier 
-    body = document.getElementById('body');
-    body.addEventListener("keydown", setup); 
-    
-	
-}
-// fonction d'initialisation du jeu
-function setup() {
-	
-	
-	//on supprime les fils du terrain, des fils peuvent être mis par la fonction presetup ou setup elle même
-    supprimerFilsTerrain(); 
-    // on initialise les variables globales body et terrain
-    body = document.getElementById('body');
-    //on supprime l'écouteur d'évènement sur le body que nous avons activé dans la fonction presetup
-    body.removeEventListener("keydown", setup); 
-    terrain = document.getElementById('terrain');
-
-    // on enleve le margin par defaut du body qui décale légèrement le jeu vers la droite et le bas
-    body.style.margin = "0px";
-
-    //
-    //  Fond
-    //
-
-	//chargement de l'image de fond
-    chargerImage('fond.jpg');
 
     //
     //  Joueur
@@ -119,88 +119,109 @@ function setup() {
 	ballon.style.left = (fond.width / 2) +5 + "px"; // au centre
     ballon.style.top = (2/3) * fond.height + 25 + "px"; // a 2/3 du bas de l'image = au centre*/
     
-   
-    //initialisation du nombre de déplacement à 0
-    nbDeplacement=0; 
+	//
+	//	Regles du jeu
+	//
     
-    // MODIFICATION 
-    
-    //position de la div contenant les règles 
-	regles.style.position="absolute"; 
-    regles.style.width= window.innerWidth /3  + "px"; 
-    regles.style.height= fond.height + "px"; 
-    regles.style.left= fond.width + "px";
-    
+	regles = document.getElementById('regles');
+
+	regles.style.position = "absolute"; 
+    regles.style.width = window.innerWidth /3  + "px"; 
+    regles.style.height = fond.height + "px"; 
+    regles.style.left = fond.width + "px";
+
     //insertion de l'image aide sur le document
     insererAide(); 
-    
-    
-    //position du bouton cacherRegles
-    var cacherRegles=document.getElementById('cacher'); 
-	
-    cacherRegles.style.position="absolute"; 
-    cacherRegles.style.left=fond.width + "px";
-    cacherRegles.style.top=(fond.height) + "px";
-    
-	//initialisation de la div contenant le score ainsi que le nombre de vies
-	TONscore.style.position="absolute"; 
-    TONscore.style.width="100"; 
-    TONscore.style.heigh="50px"; 
-	TONscore.innerHTML="Score : "+score+" Nombre de vies "+nbVie;
-    TONscore.style.top = (2/3) * fond.height + 50+ "px";
 
+	//
+	// Bouton servant a cacher les règles du jeu
+	//
+
+    boutonCacherRegles = document.getElementById('cacher'); 
+	
+    boutonCacherRegles.style.position = "absolute"; 
+    boutonCacherRegles.style.left = fond.width + "px";
+    boutonCacherRegles.style.top = fond.height + "px";
+
+	//
+	//	Meilleurs scores
+	//
+	
+	scores = document.getElementById('scores');
+
+	var nbChildren = scores.children.length //on compte les enfants de la zone
+	for(var i=0; i < nbChildren; i++) {
+		scores.removeChild(scores.lastChild); //on supprime les enfants de la zone
+	}
+
+	scores.style.position = "absolute";
+	scores.style.width = fond.width + "px";
+	scores.style.height = fond.height / 2 + "px";
+	scores.style.top = fond.height + "px";
+	
+	var scoreTitre = document.createElement("h2");
+	scoreTitre.innerHTML = " Meilleurs Scores ";
+	scoreTitre.style.textAlign = "center";
+	scores.appendChild(scoreTitre);
+	
+	// on lance une requete vers la bdd afin d'obtenir les 5 meilleurs scores, et de les afficher dans la div scores
+	scoreRequest();
+
+	//
+	//	Score actuel du joueur
+	//
+
+	TONscore = document.getElementById('tonScore');
+
+	TONscore.style.position = "absolute"; 
+    TONscore.style.width = "100"; 
+    TONscore.style.height = "50px"; 
+	TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie;
+    TONscore.style.top = (2/3) * fond.height + 50 +  "px";
+    
     //
     // Ajout des écouteurs d'événement
     // 
-    
-    document.addEventListener("keydown", appuyer); 
-   
-    cacherRegles.addEventListener("click", insererAide); 
-    
-    
-   
+
+	document.removeEventListener("keydown", traiterAppuieTouche);
+    document.addEventListener("keydown", traiterAppuieTouche);
+
+	boutonCacherRegles.addEventListener("click", insererAide);    
 }
-// MODIFICATION
+
+function traiterAppuieTouche(event) { 
+	if(afficherEcranAccueil) { 
+		afficherEcranAccueil = false;
+		setup();
+	 }
+	 else if(afficherEcranPerdu) {
+		 afficherEcranPerdu = false;
+		 afficherEcranAccueil = true;
+		 setup();
+	 }
+	 else {
+		 appuyer(event);
+	 }
+}
+
 function insererAide() {
 	//on supprime les fils de regles ce qui évitera d'avoir deux fois la même image affichée
-	var nbChildren=regles.children.length; //on compte le nombre d'enfants de règles  
-	for(var i=0; i<nbChildren; i++) {
+	var nbChildren = regles.children.length; //on compte le nombre d'enfants de règles  
+	for(var i=0; i < nbChildren; i++) {
 		regles.removeChild(regles.lastChild); //on supprime les enfants de règles
 	}
+
 	//création de l'image
-	var imgAide=document.createElement('img'); 
+	var imgAide = document.createElement('img'); 
 	//ajout d'attribut id et src
     imgAide.setAttribute('id', 'help'); 
-    imgAide.setAttribute('src','aide.jpg'); 
-    imgAide.addEventListener("mouseover",afficherAide);
-    //écouter d'évenement appelant la fonction Opacity au survol de l'image aide pour passer l'opacity des images à 0.5
+    imgAide.setAttribute('src', './aide.jpg'); 
+    imgAide.addEventListener("mouseover", afficherAide);
+    //écouteur d'évenement appelant la fonction Opacity au survol de l'image aide pour passer l'opacity des images à 0.5
     imgAide.addEventListener("mouseover", Opacity); 
-    imgAide.style.width=(1/3)*fond.width-100+"px";
+    imgAide.style.width = (1/3) * fond.width - 100 + "px";
     //insertion dans le dom
-    regles.appendChild(imgAide); 
-   
-}
-
-function getBallonX() {
-	// on récupère la variable "left" du ballon
-	var xString = ballon.style.left;
-	
-	// on récupère uniquement les "nombres" correspondant à la position en x du ballon ( c'est à dire, on enlève le "px" )
-	var xNombre =  xString.slice(0, xString.length-2);
-	
-	// on divise par un afin de retourner un nombre et pas une chaine de caractères
-	return xNombre / 1;
-}
-
-function getBallonY() {
-	// on récupère la variable "top" du ballon
-	var yString = ballon.style.top;
-	
-	// on récupère uniquement les "nombres" correspondant à la position en y du ballon ( c'est à dire, on enlève le "px" )
-	var yNombre =  yString.slice(0, yString.length-2);
-	
-	// on divise par un afin de retourner un nombre et pas une chaine de caractères
-	return yNombre / 1;
+    regles.appendChild(imgAide);  
 }
 
 function appuyer(event) {
@@ -226,103 +247,112 @@ function relacher(event, debut) {
 		var fin = event.timeStamp;
 		document.removeEventListener("keyup",function(event) { relacher(event, debut) } );
 		enCoursDeTir = false;
-		//tir(fin-debut);
+		console.log(fin-debut); // tir(fin-debut);
 	}
 }
 
-function PositionX(img) {
-	
-	
-	var X = img.style.left; //on recupère la position sur l'axe des x
-	var lx = X.length; //on récupère la taille
-	var nbX = X.slice(0,lx-2); //on prend uniquemet les nombres du tableau récupéré
-	return nbX; 
-	
+function myajax(url, callBack) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url, true);
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest); });
+    httpRequest.send(null);
 }
 
-function PositionY(variable) {
-	
-	
-	var Y= variable.style.top;  //on recupère la position sur l'axe des y 
-	
-	var ly = Y.length;//on récupère la taille
-	
-	var nbY = Y.slice(0,ly-2); //on prend uniquemet les nombres du tableau récupéré
-	
-	return nbY; 
+function scoreRequest() {
+    var url = 'http://infolimon.iutmontp.univ-montp2.fr/~tornilf/projet-js-master/scoreRequest.php?action=select';
+    myajax(url, afficherScores);
 }
 
-function bougerPersoX(event) {	
-	//récupérer la position
-	var positionX=PositionX(joueur); 
+function insererScoreBdd(nom, score) {
+    var url = 'http://infolimon.iutmontp.univ-montp2.fr/~tornilf/projet-js-master/scoreRequest.php?action=insert&nom=' + nom + '&score=' + score;
+    myajax(url, scoreRequest);
+}
+
+function afficherScores(httpRequest) {
+	var tabRep = JSON.parse(httpRequest.responseText);
+	for(var i=scores.children.length-1; i>0; i--) {
+		scores.removeChild(scores.lastChild);
+	}
+    for(var i=0; i<tabRep.length; i++) {
+		var p = document.createElement("p");
+		p.id = "score" + i;
+		p.innerHTML = tabRep[i].nomJoueur + " : " + tabRep[i].score + " points";
+		p.style.textAlign = "center";
+		scores.appendChild(p);
+    }
+}
+
+function getImageX(image) {
+	// on récupère la variable "left" du l'image
+	var xString = image.style.left;
 	
+	// on récupère uniquement les "nombres" correspondant à la position en x du ballon ( c'est à dire, on enlève le "px" )
+	var xNombreChaine =  xString.slice(0, xString.length-2);
 	
+	// on divise par un afin de retourner un nombre et pas une chaine de caractères
+	return xNombreChaine / 1;
+}
+
+// cf getImageX(image) pour les explications
+function getImageY(image) {
+	var yString = image.style.top;
+	var yNombreChaine =  yString.slice(0, yString.length-2);
+	return yNombreChaine / 1;
+}
+
+function bougerPersoX(event) {
 	if (event.keyCode === 39) {
-		if(positionX<2*fond.width/3) {
-			//on bouge le personnage vers la droite 
-			var newPosition=(Number (positionX) + 10)+"px"; 
-			joueur.style.left=newPosition;
-			ballon.style.left=(getBallonX()+10)+"px"; 
+		if(getImageX(joueur) < 2*fond.width/3) {
+
+			//on bouge le personnage vers la droite en déplaçant aussi le ballon qu'il a dans les mains
+			joueur.style.left = getImageX(joueur) + 10 + "px";
+			ballon.style.left = getImageX(ballon) +10 + "px";
+
 			//on augmente le nombre de déplacement
-			nbDeplacement=nbDeplacement+1;  
+			nbDeplacement ++;  
+
 			if(nbDeplacement>50) {
 				PerteVie(); //on perd une vie
-				
 			}
 		} 
-		
-		
 	}
-	if(event.keyCode===37) {
-		//on bouge le personnage vers la gauche 
-		if( positionX >fond.width/4) {
-			var newPosition=(Number (positionX) -10)+"px"; 
-		
-			joueur.style.left=newPosition; 
-			ballon.style.left=(getBallonX()-10)+"px"; 
+	if(event.keyCode === 37) { 
+		if(getImageX(joueur) > fond.width/4) {
+
+			//on bouge le personnage vers la gauche en déplaçant aussi le ballon qu'il a dans les mains
+			joueur.style.left= getImageX(joueur) - 10 + "px"; 
+			ballon.style.left= getImageX(ballon) - 10 + "px";
+
 			//on augmente le nombre de déplacement
-			nbDeplacement=nbDeplacement+1; 
+			nbDeplacement ++;
+
 			if(nbDeplacement>50) {
 				PerteVie(); //on perd une vie
-				
-				
 			}
 		}
-	}
-	
-	
+	}	
 }
-
 
 function afficherAide(event) {
 	//on cache l'image d'aide
-	event.target.style.visibility="hidden"; 
-	//on crée puis isère un h2 dans le dom fils de regles
+	event.target.style.visibility = "hidden"; 
+	//on crée puis insère un h2 dans le dom fils de regles
 	var newh2=document.createElement('h2'); 
 	newh2.innerHTML="Voici les règles de notre jeu"; 
 	regles.appendChild(newh2); 
 	
 	//on crée et on insère des paragraphes dans le dom
 	var newp=document.createElement('p'); 
-	newp.innerHTML="Pour déplacer appuyer sur les flèches de droite ou de gauche"; 
+	newp.innerHTML="Pour déplacer le joueur, appuyez sur les flèches de droite ou de gauche"; 
 	regles.appendChild(newp); 
 	
 	var newp2=document.createElement('p'); 
-	newp2.innerHTML="Pour tirer appuyer sur la touche w, plus vous appuyer plus le tir est performant"; 
+	newp2.innerHTML="Pour tirer, appuyez sur la touche w, plus vous laissez appuyé, plus le tir est puissant"; 
 	regles.appendChild(newp2); 
 	//on supprime le premier fils de règle pour ne plus avoir l'image dans la div règle
 	regles.removeChild(regles.firstChild); 
 	 
-}
-
-
-
-function supprimerFilsTerrain() {
-
-	var nbChildren=terrain.children.length //on compte les enfants de la zone
-	for(var i=0; i<nbChildren; i++) {
-		terrain.removeChild(terrain.lastChild); //on supprime les enfants de la zone
-	}
 }
 
 function Opacity() {
@@ -342,19 +372,19 @@ function OpacityAllImage() {
 
 function faitPanier() {
 	//on récupère la position du ballon sur l'axe des x et sur l'axe des y
-	var positionXballon=PositionX(ballon); 
-	var positionYballon=PositionY(ballon); 
+	var positionXballon = getImageX(ballon); 
+	var positionYballon = getImageY(ballon); 
 	
 	//on regarde si le ballon est proche du panier 
-	if (120 <positionXballon && positionXballon<160 && positionYballon>85 && positionYballon<105) {
-			score=score+1; //on augmente le score 
-			TONscore.innerHTML="Score : "+score+" Nombre de vies "+nbVie; //on l'affiche
+	if (120 < positionXballon && positionXballon < 160 && positionYballon > 85 && positionYballon < 105) {
+			score ++; //on augmente le score 
+			TONscore.innerHTML="Score : " + score + " Nombre de vies " + nbVie; //on l'affiche
 			setup(); 
 			
 	}
-	else if (700<positionXballon && positionXballon<740 && positionYballon>85 && positionYballon<105) {
-			score=score+1; 
-			TONscore.innerHTML="Score : "+score+" Nombre de vies "+nbVie; //on l'affiche
+	else if (700 < positionXballon && positionXballon < 740 && positionYballon > 85 && positionYballon < 105) {
+			score ++; 
+			TONscore.innerHTML="Score : " + score + " Nombre de vies " + nbVie; //on l'affiche
 			setup(); 
 		
 	}
@@ -363,28 +393,19 @@ function faitPanier() {
 	}
 }
 
-	
 function PerteVie() { 
 	alert("perte d'une vie"); 
-	nbVie=nbVie-1; //on décrémente le compteur 
-	TONscore.innerHTML="Score : "+score+" Nombre de vies "+nbVie;//on affiche
-	if(nbVie==0) {
-		
+	nbVie --; //on décrémente le compteur 
+	TONscore.innerHTML="Score : " + score + " Nombre de vies " + nbVie;//on affiche
+	if(nbVie === 0) {
 		finGame();//on appelle la fonction mettant fin au jeu 
 	} 
 	else {
 		setup();
 	}
-	
-	
 }
 
-
-
 function finGame() {
-	supprimerFilsTerrain(); //on supprime les fils du terrain
-	chargerImage('game.png'); //on affiche l'image de fin du jeu
-	body.addEventListener("keydown", setup); //on place un écouter d'évenement qui relance le jeu si on appuie sur une touche
-	nbVie=5; 
-	score=0;
+	afficherEcranPerdu = true;
+	setup();
 }
