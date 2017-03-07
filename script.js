503c7a30cd1a6a8ce960204a6eb2707bc007a307
Florent Tornil <flo_tornil34@hotmail.fr>
2017-03-07 19:22:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,16 +9,15 @@ var ballon; 			// variable correspondant au ballon (img), servant à simplifier 
 var scores; 			// variable correspondant aux 5 meilleurs scores (div), servant à simplifier la lecture du code
 var regles; 			// variable correspondant aux regles (div), servant à simplifier la lecture du code
 var boutonCacherRegles	// variable correspondant au bouton permettant de cacher les règles du jeu (input), servant à simplifier la lecture du code
-var TONscore; 			// variable correspondant au score actuel du joueur (div), servant a simplifier la lecture du code
+var information; 			// variable correspondant au score actuel du joueur (div), servant a simplifier la lecture du code
 
 var afficherEcranAccueil = true;	// booleen vrai ssi il faut afficher l'écran d'accueil dans le setup au lieu de l'image de fond du terrain
 var afficherEcranPerdu = false;		// boolean vrai ssi il faut afficher l'écran gameOver dans le setup au lieu de l'image de fond du terrain
 
-var nbDeplacement; 	// nombre de déplacement du personnage
-var score;  		// score actuel du joueur
-var nbVie; 			// nombre de vies restantes au joueur ( = nombres de panier ouvant etre ratées avant d'avoir perdu )
+var perdUneVie = true; // vrai ssi le joueur perd une vie lorsque le ballon s'arrete, permet d'éviter de perdre une vie en marquant un panier
 
 var enCoursDeTir = false; 	// vrai ssi le joueur est en train de tirer (touche enfoncée)
+var peutBouger = true; 	// vrai ssi le joueur est en train de tirer (touche enfoncée)
 
 var chaineTriche=""; //variable contenant les caractères que nous saississons au clavier
 var aTricher=false; //au début du jeu, le joueur n'a pas triché
@@ -42,17 +41,6 @@ function setup() {
 
     // on enleve le margin par defaut du body qui décale légèrement le jeu vers la droite et le bas
     body.style.margin = "0px";
-
-	//
-	// Initialisation des variables déplacement et vies
-	//
-
-		nbDeplacement=50;
-	if(afficherEcranAccueil) {
-		score = 0;
-		nbVie = 5;
-		
-	}
 
     //
     //  Fond
@@ -127,6 +115,17 @@ function setup() {
 	ballon.style.left = (fond.width / 2) +5 + "px"; // au centre
     ballon.style.top = (2/3) * fond.height + 25 + "px"; // a 2/3 du bas de l'image = au centre*/
     
+    //
+	//	Informations
+	//
+
+	if(afficherEcranAccueil || afficherEcranPerdu) {
+		information = new informations(300, 100);
+		information.div.removeAttribute("class");
+	}
+	else {
+		information.afficher();
+	}
 	//
 	//	Regles du jeu
 	//
@@ -175,18 +174,6 @@ function setup() {
 	
 	// on lance une requete vers la bdd afin d'obtenir les 5 meilleurs scores, et de les afficher dans la div scores
 	scoreRequest();
-
-	//
-	//	Score actuel du joueur
-	//
-
-	TONscore = document.getElementById('tonScore');
-
-	TONscore.style.position = "absolute"; 
-    TONscore.style.width = "300px"; 
-    TONscore.style.height = "50px"; 
-	TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-    TONscore.style.top = (2/3) * fond.height + 100+  "px";
     
     //
     // Ajout des écouteurs d'événement
@@ -197,15 +184,17 @@ function setup() {
 	document.addEventListener("keydown", triche);
 	boutonCacherRegles.addEventListener("click", insererAide);    
 	 
-	
-	//div de triche
+	//
+	//	div de triche
+	//
 	
 	tricheur.style.position = "absolute"; 
     tricheur.style.width = "500px"; 
     tricheur.style.height = "50px"; 
     tricheur.style.fontWeight="bold"; 
     
-	tricheur.style.left ="200px"; // au centre
+	tricheur.style.left = fond.width/2 -250 + "px";
+	tricheur.style.top = fond.height/2 - 100 + "px";
    
     
 }
@@ -268,9 +257,9 @@ function relacher(event, debut) {
 		var fin = event.timeStamp;
 		document.removeEventListener("keyup",function(event) { relacher(event, debut) } );
 		enCoursDeTir = false;
-		console.log(fin-debut); 
-		tir(fin-debut);
-		
+		if(peutBouger) {
+			tir(fin-debut);
+		}	
 	}
 }
 
@@ -288,7 +277,7 @@ function scoreRequest() {
 }
 
 function insererScoreBdd(nom, score) {
-    var url = 'http://infolimon.iutmontp.univ-montp2.fr/~tornilf/projet-js-master/scoreRequest.php?action=insert&nom=' + nom + '&score=' + score;
+    var url = 'http://infolimon.iutmontp.univ-montp2.fr/~tornilf/projet-js-master/scoreRequest.php?action=insert&nom=' + nom + '&score=' + information.score;
     myajax(url, scoreRequest);
 }
 
@@ -325,7 +314,7 @@ function getImageY(image) {
 }
 
 function bougerPersoX(event) {
-	if (event.keyCode === 39) {
+	if (event.keyCode === 39 && peutBouger) {
 		if(getImageX(joueur) < 2*fond.width/3) {
 
 			//on bouge le personnage vers la droite en déplaçant aussi le ballon qu'il a dans les mains
@@ -333,14 +322,14 @@ function bougerPersoX(event) {
 			ballon.style.left = getImageX(ballon) +10 + "px";
 
 			//on augmente le nombre de déplacement
-			nbDeplacement --;  
-			TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-			if(nbDeplacement==0) {
+			information.nbDeplacement --;  
+			information.afficher();
+			if(information.nbDeplacement === 0) {
 				PerteVie(); //on perd une vie
 			}
 		} 
 	}
-	if(event.keyCode === 37) { 
+	if(event.keyCode === 37 && peutBouger) { 
 		if(getImageX(joueur) > fond.width/4) {
 
 			//on bouge le personnage vers la gauche en déplaçant aussi le ballon qu'il a dans les mains
@@ -348,9 +337,9 @@ function bougerPersoX(event) {
 			ballon.style.left= getImageX(ballon) - 10 + "px";
 
 			//on augmente le nombre de déplacement
-			nbDeplacement --;
-			TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-			if(nbDeplacement==0) {
+			information.nbDeplacement --;  
+			information.afficher();
+			if(information.nbDeplacement === 0) {
 				PerteVie(); //on perd une vie
 			}
 		}
@@ -399,29 +388,22 @@ function faitPanier() {
 	var positionXballon = getImageX(ballon); 
 	var positionYballon = getImageY(ballon); 
 	
+	var panierX = 4*fond.width/5 + 10;
+	var panierY = 2*fond.height/5;
+	
 	//on regarde si le ballon est proche du panier 
-	if (120 < positionXballon && positionXballon < 160 && positionYballon > 85 && positionYballon < 105) {
-			score ++; //on augmente le score 
-			TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-			setup(); 
-			
-	}
-	else if (700 < positionXballon && positionXballon < 740 && positionYballon > 85 && positionYballon < 105) {
-			score ++; 
-			TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-			setup(); 
-		
-	}
-	else {
-		PerteVie(); //on perd une vie 
+	if (panierX < positionXballon && positionXballon < panierX+100 && panierY < positionYballon  && positionYballon < panierY+20) {
+			information.score ++; //on augmente le score 
+			information.afficher();
+			perdUneVie = false;
 	}
 }
 
 function PerteVie() { 
 	alert("perte d'une vie"); 
-	nbVie --; //on décrémente le compteur 
-	TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-	if(nbVie === 0) {
+	information.nbVie --; //on décrémente le compteur 
+	information.afficher();
+	if(information.nbVie === 0) {
 		finGame();//on appelle la fonction mettant fin au jeu 
 	} 
 	else {
@@ -431,15 +413,11 @@ function PerteVie() {
 
 function finGame() {
 	afficherEcranPerdu = true;
-	
+	// inserer bdd ici
 	setup();
 }
 
-
-
 function triche(event) {
-	
-	
 	chaineTriche=chaineTriche+event.keyCode;  //on ajoute dans la chaine le code des touches enfoncées
 	var code = "66" + "65" + "83" + "75" + "69" + "84"; //chaine représentant le code triche 
 	
@@ -451,27 +429,25 @@ function triche(event) {
 		feu.style.position="absolute"; 
 		feu.style.height="200px"; 
 		feu.style.width="150px"; 
-		feu.style.top="200px"; 
+		feu.style.top=getImageY(joueur) - 100 + "px"; 
 		feu.style.left=joueur.style.left; 
 		terrain.appendChild(feu);
 		document.removeEventListener("keydown", traiterAppuieTouche);
 	}
 	if (chaineTriche.indexOf(code,0) != -1 && aTricher) {//on vérifie que chaineTriche contient le code triche 
-		score=0; 
+		information.score=0; 
 		alert(" Trop de triche score = 0 !"); 
-		TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
-		
+		information.afficher();
 	}
 	else if (chaineTriche.indexOf(code,0) != -1) {
-		score=score+10;//on  augmente le score
+		information.score+=10;//on  augmente le score
 		tricheur.innerHTML="Si vous trichez encore <br> Votre score va retomber à 0 ! </br"; //écriture du message
 		tricheur.style.textAlign="center"; 
 		aTricher=true; 
 		setTimeout(effacerAlerteTriche, 5000);
-		TONscore.innerHTML = "Score : " + score + " Nombre de vies " + nbVie+" Déplacement Restant "+nbDeplacement; 
+		information.afficher();
 		chaineTriche=""; //on réinitialise la chaine pour ne pas pouvoir tricher plusieurs fois
 	} 
-	
 }
 
 function effacerAlerteTriche() {
@@ -483,18 +459,14 @@ function effacerAlerteTriche() {
 	document.addEventListener("keydown", traiterAppuieTouche);
 }
 
+function tir(force){//fonction lancé dès que la touche tir est relaché	
+	peutBouger = false;	
+    if(force > 2500){
+		force = 2500;
+	}
 
-
-
-function tir(force){//fonction lancé dès que la touche tir est relaché
-		
-        if(force > 2500){
-            force = 2500;}
-
-        var attraction = 1;
-        lancer = setInterval(function() {attraction += 2 ;intervalle(force, attraction);arret()}, 75);
-        
-        
+	var attraction = 1;
+	lancer = setInterval(function() {attraction += 2 ;intervalle(force, attraction);arret()}, 75);    
 }
 
 function intervalle(force, attraction){// fonction qui a chaque intervalle de temps bouge le ballon.
@@ -502,11 +474,94 @@ function intervalle(force, attraction){// fonction qui a chaque intervalle de te
     ballon.style.top = getImageY(ballon) - (8+(force*0.024)) + attraction + "px";
     ballon.style.left = getImageX(ballon) + (6+(force*0.0047)) + "px";
     
-}
-function arret(){
-
-    if(ballon.style.top.substr(0,3) >= 490  || ballon.style.left.substr(0,3) >= 950){
-        clearInterval(lancer);
-    }
+    faitPanier();
     
 }
+
+function arret(){
+	
+	var panierX = 4*fond.width/5 + 10;
+	var panierY = 2*fond.height/5;
+	
+    if(getImageX(ballon) > panierX+100 || getImageY(ballon) > getImageY(joueur)+100 ) {
+		
+		clearInterval(lancer);
+			
+		joueur.style.left = (fond.width / 2) - 50 + "px"; // au centre
+		joueur.style.top = (2/3) * fond.height -25 + "px"; // a 2/3 du bas de l'image = au centre
+		
+		ballon.style.left = (fond.width / 2) +5 + "px"; // au centre
+		ballon.style.top = (2/3) * fond.height + 25 + "px"; // a 2/3 du bas de l'image = au centre*/
+		
+		information.nbDeplacement = 50;
+		if(perdUneVie) {
+			PerteVie();
+		}
+		perdUneVie = true;
+		peutBouger = true;
+		setup(); 	
+    }    
+}
+
+function informations(w, h) {
+	
+	this.div = document.getElementById('informations');
+		
+	this.width = w;
+	this.height = h;
+	
+	this.top = 50;
+	this.left = fond.width/2 - this.width/2;
+	
+	this.score = 0;
+	this.nbDeplacement = 50;
+	this.nbVie = 5;
+	
+	this.iteration = 0;
+	this.intervalle;
+	
+	this.afficher = function() {
+		this.div.style.position = "absolute";
+		this.div.style.width = this.width + "px";
+		this.div.style.height = this.height + "px";
+		this.div.style.top = this.top + "px";
+		this.div.style.left = this.left + "px";
+		this.div.setAttribute("class","infos");
+		this.div.innerHTML = "<p class=score>" + this.score + "</p><p> <span class=gauche>Vies</span><span class=droite>Pas</span> </p><p class=chiffres> <span class=gauche>" + this.nbVie + "</span><span class=droite> " + this.nbDeplacement + "</span></p>";	
+	}
+	
+	this.bouger = function() {
+		this.iteration = 0;
+		this.intervalle = setInterval(function() { information.actionDeIntervalle() }, 30);		
+	}
+	
+	this.actionDeIntervalle = function(x, y) {
+		if(this.iteration < 5) {
+			this.deplacer(-10,0);
+		}
+		else if(this.iteration < 10) {
+			this.deplacer(0,10);
+		}
+		else if (this.iteration < 15) {
+			this.deplacer(10,0);
+		}
+		else if (this.iteration < 20) {
+			this.deplacer(0,-10);
+		}
+		else {
+			clearInterval(this.intervalle);
+		}
+		this.iteration++;
+	}
+	
+	this.deplacer = function(x, y) {
+		this.left+= x;
+		this.top += y;
+		this.afficher();
+	}	
+}
+	
+
+
+
+
